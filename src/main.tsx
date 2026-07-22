86b6cf886d928a15805c92348c45363981ebafa4
1111 <3337836675@qq.com>
2026-07-22 22:49:47 +0800

    fix: remove ToastProvider wrapper to resolve login failure

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,10 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./styles/design-tokens.css";
 import "./index.css";
-import { ToastProvider } from "./components/ui/Toast";
 import PlatformApp from "./PlatformApp";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <ToastProvider>
-      <PlatformApp />
-    </ToastProvider>
+    <PlatformApp />
   </StrictMode>
 );
